322b2d5ae19741bde11540bcf5c473a417f521b8
Aguss0152 <thewolf0152@gmail.com>
2026-04-18 02:45:35 -0300

    Restaurar los archivos del proyecto

--- secrets.example.js
+++
@@ -1,5 +0,0 @@
-// Completar con tus datos reales y renombrar a secrets.js
-
-window.APP_CONFIG = {
-    SHEET_URL: ''
-};
---
+++ secrets.js
@@ -0,0 +1,3 @@
+window.APP_CONFIG = {
+    SHEET_URL: 'https://docs.google.com/spreadsheets/d/1pu71V9gFGbaJHSM-L6pa6CaTWvgz4Hpb7jzrjLiX8TY/export?format=csv'
+};
